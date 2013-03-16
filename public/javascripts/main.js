d6866dec6bcdb0c7bb029b0b4b11d6e31e4e103e
Dominik Kundel <dominik.kundel@me.com>
2013-03-16 22:47:16 +0100

    adding redirect when creating event

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -75,7 +75,7 @@ const SERVER = "http://localhost:3000";
 			info.picture_path = ticketfever._header_img;
 			$.ajax("/createEvent", {data: info, type: "POST", success: function(error, result){
 				if(!error) {
-					console.log(result);
+					window.location = SERVER+"/"+result.result.event_nickname;
 				}
 			}});
 		},
